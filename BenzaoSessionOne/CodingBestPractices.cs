11e57ebb4114ce1fc299445f46e8fa6277e5914c
prokelly <kellyrobot247@gmail.com>
2022-11-09 11:01:41 +0100

    Add project files.

---
+++ BenzaoSessionOne/CodingBestPractices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenzaoSessionOne
+{
+    internal class CodingBestPractices
+    {
+        public static void CodingGoodAndBadPractices()
+        {
+            //TO DO APP
+            Console.WriteLine("What do you have to do this week");
+            Console.WriteLine();
+            Console.WriteLine("Enter Date");
+        }
+    }
+}
---
+++ BenzaoSessionOne/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+using BenzaoSessionOne;
+namespace BenzaoSessionOne.Runner
+{
+ internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // StringMasker.Masker();
+            //CapitalLetterCheck.CheckUpperCase();
+            //Authentication.UserAuthentication();
+            //BreakAndContinue.Case();
+            //SwitchCaseStatement.SwitchCase();
+            Date.PrintDateAfterWeek();
+
+        }
+    }
+}
